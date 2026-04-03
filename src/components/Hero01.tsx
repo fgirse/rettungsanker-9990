35c5203e03f6666f7cad326134797809003da32a
fgirse <fgirs@bluewin.ch>
2026-04-03 18:51:08 +0200

    modify navigation

---
+++ src/components/Hero01.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import Spline from '@splinetool/react-spline'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/Assets/SVG/5555.svg')] bg-contain bg-no-repeat bg-center">
+      <Spline scene="https://prod.spline.design/lCFZfRh2xWNU2jdd/scene.splinecode" />
+    </main>
+  )
+}
