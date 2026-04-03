dee2f5d6dac670041897121e0702b4e9b0467b8c
fgirse <fgirs@bluewin.ch>
2026-04-04 00:11:15 +0200

    hero modified

--- src/components/Hero01.tsx
+++ src/components/Hero01.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import Spline from '@splinetool/react-spline'
+import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/Assets/SVG/5555.svg')] bg-contain bg-no-repeat bg-center">
-      <Spline scene="https://prod.spline.design/xu7v7QKyUA-MAi-P/scene.splinecode"  />
+      <Spline
+        scene="https://prod.spline.design/aiMa8ypM2n-Dl8eU/scene.splinecode" 
+      />
     </main>
   )
 }
-
