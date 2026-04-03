c96700c24818c3edc3c6eb31fcefd57903b6ef0a
fgirse <fgirs@bluewin.ch>
2026-04-03 23:02:37 +0200

    fix hero

--- src/components/Hero01.tsx
+++ src/components/Hero01.tsx
@@ -5,7 +5,8 @@ import Spline from '@splinetool/react-spline'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/Assets/SVG/5555.svg')] bg-contain bg-no-repeat bg-center">
-      <Spline scene="https://prod.spline.design/lCFZfRh2xWNU2jdd/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/xu7v7QKyUA-MAi-P/scene.splinecode"  />
     </main>
   )
 }
+
