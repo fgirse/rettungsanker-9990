83a8f6d5f278f1d6b6d91b8fb997e84d296ed559
fgirse <fgirs@bluewin.ch>
2026-04-07 08:13:00 +0200

    modify hero logo position (mt-3)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ function Hero() {
     <section className='-mt-[5vh] bg-[url("/Assets/Img/lighthouse.png")] bg-contain bg-no-repeat bg-center lg:mt-0 lg:bg-[url("/Assets/Svg/5555.svg")] lg:bg-contain lg:bg-no-repeat lg:bg-center h-screen w-full flex flex-col items-center justify-center'>
       <div className="flex flex-col items-center">
         <div className="relative top-[-30vh] w-[74vw] md:w-[70vw] lg:hidden">
-          <Image src={LogoNeu} alt="LogoAlt" className=" " />
+          <Image src={LogoNeu} alt="LogoAlt" className="mt-3 " />
         </div>
         <HeroAnimatedText />
       </div>
