84388c231849eec06cde21bba21abe1a5a6abeca
fgirse <fgirs@bluewin.ch>
2026-05-10 22:58:02 +0200

    implement  *hans_flying_music.mp4" in Hero on desktop view

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ import HeroAnimatedText from '@/components/HeroAnimatedText'
 
 function Hero() {
   return (
-    <section className='-mt-[5vh] relative overflow-hidden lg:mt-0 lg:bg-[url("/Assets/Svg/5555.svg")] lg:bg-contain lg:bg-no-repeat lg:bg-center h-screen w-full flex flex-col items-center justify-center'>
+    <section className="-mt-[5vh] relative overflow-hidden lg:mt-0 h-screen w-full flex flex-col items-center justify-center">
       {/* Background video for mobile and tablet only */}
       <video
         className="absolute inset-0 w-full h-full object-cover lg:hidden -z-10"
@@ -17,6 +17,16 @@ function Hero() {
         playsInline
       >
         <source src="/Assets/Img/lighthouse_animated.mp4" type="video/mp4" />
+      </video>
+      {/* Background video for desktop only */}
+      <video
+        className="absolute inset-0 w-full h-full object-cover hidden lg:block -z-10"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src="/Assets/Img/hans_flying_music.mp4" type="video/mp4" />
       </video>
       <div className="relative z-10 flex flex-col items-center">
         <div className="relative top-[-26vh] w-[74vw] md:w-[70vw] lg:hidden">
